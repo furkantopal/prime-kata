368ab5b431176f886091fe72ad305de8c2bdc2f3
Furkan Topal <topallfurkan@gmail.com>
2022-01-28 15:24:44 +0100

    mob next [ci-skip] [ci skip] [skip ci]

---
+++ src/test/prime.test.ts
@@ -0,0 +1,8 @@
+import { Prime } from "../main/prime";
+
+describe("Prime test", () => {
+  it("should give prime factors of 1", () => {
+    let prime: Prime = new Prime();
+    expect(prime.getFactors(1)).toEqual([]);
+  });
+});
